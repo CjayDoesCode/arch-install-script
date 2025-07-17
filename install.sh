7d744a1b2b13e88005145b8508ad354e9e967ed5
CjayDoesCode <cjaycodes@protonmail.com>
2025-07-17 19:00:35 +0800

    refactor: update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,63 @@
 #!/usr/bin/env bash
 
 # ------------------------------------------------------------------------------
+#       constants
+# ------------------------------------------------------------------------------
+
+readonly BASE_SYSTEM_PACKAGES=(
+  'base'
+  'bash'
+  'bash-completion'
+  'linux'
+  'linux-firmware'
+  'man-db'
+  'man-pages'
+  'networkmanager'
+  'pacman-contrib'
+  'reflector'
+  'sudo'
+  'texinfo'
+)
+
+readonly FILESYSTEM_UTILITY_PACKAGES=(
+  'dosfstools'
+  'e2fsprogs'
+  'exfatprogs'
+  'ntfs-3g'
+)
+
+readonly COMMON_DRIVER_PACKAGES=('mesa' 'xorg-server')
+readonly AMD_DRIVER_PACKAGES=('vulkan-radeon')
+readonly INTEL_DRIVER_PACKAGES=('vulkan-intel')
+
+readonly PIPEWIRE_PACKAGES=(
+  'pipewire'
+  'pipewire-alsa'
+  'pipewire-audio'
+  'pipewire-jack'
+  'pipewire-pulse'
+  'wireplumber'
+)
+
+readonly OPTIONAL_PACKAGES=(
+  'base-devel'
+  'git'
+  'openssh'
+  'sof-firmware'
+)
+
+declare -Ar COLOR_CODES=(
+  [black]=30
+  [red]=31
+  [green]=32
+  [yellow]=33
+  [blue]=34
+  [magenta]=35
+  [cyan]=36
+  [white]=37
+)
+
+# ------------------------------------------------------------------------------
 #       main function
 # ------------------------------------------------------------------------------
 
@@ -9,6 +66,7 @@ main() {
 
   # ----  configuration  -------------------------------------------------------
 
+  local editor_package='nano'
   local swap='true'
   local install_filesystem_utility_packages='true'
   local install_driver_packages='true'
@@ -22,47 +80,11 @@ main() {
   local swap_size=''
 
   local reflector_options=()
-
   local system_packages=()
-  local base_system_packages=(
-    'base'
-    'bash'
-    'bash-completion'
-    'linux'
-    'linux-firmware'
-    'man-db'
-    'man-pages'
-    'nano'
-    'networkmanager'
-    'pacman-contrib'
-    'reflector'
-    'sudo'
-    'texinfo'
-  )
+  local driver_packages=()
+  local optional_packages=()
 
-  local filesystem_utility_packages=(
-    'dosfstools'
-    'e2fsprogs'
-    'exfatprogs'
-    'ntfs-3g'
-  )
-
-  local common_driver_packages=('mesa' 'xorg-server')
-  local amd_driver_packages=('vulkan-radeon')
-  local intel_driver_packages=('vulkan-intel')
-
-  local pipewire_packages=(
-    'pipewire'
-    'pipewire-alsa'
-    'pipewire-audio'
-    'pipewire-jack'
-    'pipewire-pulse'
-    'wireplumber'
-  )
-
-  local optional_packages=('base-devel' 'git' 'openssh' 'sof-firmware')
-
-  system_packages+=("${base_system_packages[@]}")
+  system_packages+=("${BASE_SYSTEM_PACKAGES[@]}")
 
   case "$(get_vendor_id)" in
   AuthenticAMD)
@@ -75,72 +97,87 @@ main() {
     ;;
   *)
     print --color red 'error: unknown cpu vendor.\n\n' >&2
-    exit 1
+    return 1
     ;;
   esac
 
   if [[ "${install_filesystem_utility_packages}" == 'true' ]]; then
-    system_packages+=("${filesystem_utility_packages[@]}")
+    system_packages+=("${FILESYSTEM_UTILITY_PACKAGES[@]}")
   fi
 
+  system_packages+=("${editor_package}")
+
   if [[ "${install_driver_packages}" == 'true' ]]; then
-    system_packages+=("${common_driver_packages[@]}")
+    system_packages+=("${COMMON_DRIVER_PACKAGES[@]}")
   fi
 
   if [[ "${install_pipewire_packages}" == 'true' ]]; then
-    system_packages+=("${pipewire_packages[@]}")
+    system_packages+=("${PIPEWIRE_PACKAGES[@]}")
   fi
 
   # ----  checks  --------------------------------------------------------------
 
   if ! is_uefi; then
-    print --color red 'error: system is not booted in uefi mode.\n\n' >&2
-    exit 1
+    print --color red 'error: system not booted in uefi mode.\n\n' >&2
+    return 1
   fi
 
   if ! is_connected; then
-    print --color red 'error: failed to connect to the internet.\n\n' >&2
-    exit 1
+    print --color red 'error: unable to connect to the internet.\n\n' >&2
+    return 1
   fi
 
-  if [[ ! -e "$(dirname "$0")/configure.sh" ]]; then
-    print --color red 'error: failed to find configure.sh.\n\n' >&2
-    exit 1
+  if [[ ! -e "${BASH_SOURCE%/*}/configure.sh" ]]; then
+    print --color red "error: 'configure.sh' not found.\\n\\n" >&2
+    return 1
   fi
 
   # ----  input  ---------------------------------------------------------------
 
-  {
-    read -r target_disk
-    read -r root_partition
-    read -r boot_partition
-  } < <(input_target_disk)
+  target_disk="$(input_target_disk)" || return 1
+
+  case "${target_disk}" in
+  /dev/sd*)
+    root_partition="${target_disk}2"
+    boot_partition="${target_disk}1"
+    ;;
+  /dev/nvme*)
+    root_partition="${target_disk}p2"
+    boot_partition="${target_disk}p1"
+    ;;
+  /dev/mmcblk*)
+    root_partition="${target_disk}p2"
+    boot_partition="${target_disk}p1"
+    ;;
+  *)
+    print --color red 'error: invalid disk.\n\n' >&2
+    return 1
+    ;;
+  esac
 
   if [[ "${swap}" == 'true' ]]; then
-    read -r swap_size < <(input_swap_size)
+    swap_size="$(input_swap_size)"
   fi
 
   readarray -t reflector_options < <(input_reflector_options)
 
-  if [[ "${install_driver_packages}" == 'true' ]]; then
-    if [[ "$(confirm 'install amd driver packages?')" == 'true' ]]; then
-      system_packages+=("${amd_driver_packages[@]}")
-    fi
-
-    if [[ "$(confirm 'install intel driver packages?')" == 'true' ]]; then
-      system_packages+=("${intel_driver_packages[@]}")
-    fi
+  if [[ "${#reflector_options[@]}" -eq 0 ]]; then
+    return 1
   fi
 
-  for package in "${optional_packages[@]}"; do
-    if [[ "$(confirm "install ${package}?")" == 'true' ]]; then
-      system_packages+=("${package}")
-    fi
-  done
+  if [[ "${install_driver_packages}" == 'true' ]]; then
+    readarray -t driver_packages < <(input_driver_packages)
+    system_packages+=("${driver_packages[@]}")
+  fi
 
-  print --color red 'warning: installation will wipe target disk.\n'
-  if [[ "$(confirm 'proceed with installation?')" != 'true' ]]; then
-    exit 0
+  readarray -t optional_packages < <(input_optional_packages)
+  system_packages+=("${optional_packages[@]}")
+
+  declare -p
+
+  print --color yellow 'warning: installation will wipe target disk.\n\n'
+  if ! confirm 'proceed with installation?'; then
+    return 0
   fi
 
   # ----  installation  --------------------------------------------------------
@@ -149,58 +186,58 @@ main() {
     print --color cyan 'info: attempting to sync system clock...\n\n'
     sync_clock || {
       print --color red 'error: failed to sync system clock.\n\n' >&2
-      exit 1
+      return 1
     }
   fi
 
   print --color cyan 'info: partitioning disk...\n\n'
   partition_disk "${target_disk}" || {
     print --color red 'error: failed to partition disk.\n\n' >&2
-    exit 1
+    return 1
   }
 
   print --color cyan 'info: formatting partitions...\n\n'
   format_partitions "${root_partition}" "${boot_partition}" || {
     print --color red 'error: failed to format partitions.\n\n' >&2
-    exit 1
+    return 1
   }
 
   print --color cyan 'info: mounting file systems...\n\n'
   mount_file_systems "${root_partition}" "${boot_partition}" || {
     print --color red 'error: failed to mount file systems.\n\n' >&2
-    exit 1
+    return 1
   }
 
   if [[ "${swap}" == 'true' ]]; then
     print --color cyan 'info: creating swap...\n\n'
     create_swap "${swap_size}" || {
       print --color red 'error: failed to create swap.\n\n' >&2
-      exit 1
+      return 1
     }
   fi
 
   print --color cyan 'info: updating mirror list...\n\n'
   update_mirror_list "${reflector_options[@]}" || {
     print --color red 'error: failed to update mirror list.\n\n' >&2
-    exit 1
+    return 1
   }
 
   print --color cyan 'info: installing system packages...\n\n'
   install_system_packages "${system_packages[@]}" || {
     print --color red 'error: failed to install system packages.\n\n' >&2
-    exit 1
+    return 1
   }
 
   print --color cyan 'info: generating fstab...\n\n'
   generate_fstab || {
     print --color red 'error: failed to generate fstab.\n\n' >&2
-    exit 1
+    return 1
   }
 
   print --color cyan 'info: changing root to new system...\n\n'
   configure_system || {
     print --color red 'error: failed to configure the system.\n\n' >&2
-    exit 1
+    return 1
   }
 
   print --color cyan 'info: installation completed.\n\n'
@@ -217,16 +254,14 @@ confirm() {
   while true; do
     print --color cyan "${prompt} [y/n]: " >&2
     read -r input
-    printf '\n' >&2
+    print '\n' >&2
 
     case "${input,,}" in
     y | yes)
-      printf 'true'
-      break
+      return 0
       ;;
     n | no)
-      printf 'false'
-      break
+      return 1
       ;;
     *)
       print --color red 'error: invalid input. try again.\n\n' >&2
@@ -237,14 +272,12 @@ confirm() {
 
 input_target_disk() {
   local target_disk=''
-  local root_partition=''
-  local boot_partition=''
-
   local disks=''
-  if ! disks="$(list_disks)"; then
+
+  disks="$(list_disks)" || {
     print --color red 'error: failed to get disks.\n\n' >&2
-    exit 1
-  fi
+    return 1
+  }
 
   print --color cyan 'disks:\n' >&2
   printf '%s' "${disks}" | sed 's/^/  - /' >&2
@@ -253,46 +286,16 @@ input_target_disk() {
   while true; do
     print --color cyan 'enter target disk (e.g., "/dev/sda"): ' >&2
     read -r target_disk
-    printf '\n' >&2
+    print '\n' >&2
 
-    target_disk="${target_disk,,}"
-
-    if is_disk_valid "${target_disk}"; then
-      case "${target_disk}" in
-      /dev/sd*)
-        root_partition="${target_disk}2"
-        boot_partition="${target_disk}1"
-        break
-        ;;
-      /dev/nvme*)
-        root_partition="${target_disk}p2"
-        boot_partition="${target_disk}p1"
-        break
-        ;;
-      /dev/mmcblk*)
-        root_partition="${target_disk}p2"
-        boot_partition="${target_disk}p1"
-        break
-        ;;
-      *)
-        print --color red 'error: invalid disk. try again.\n\n' >&2
-        ;;
-      esac
-    elif [[ "$?" -eq 2 ]]; then
-      print --color red 'error: failed to get disks.\n\n' >&2
-      exit 1
+    if target_disk="$(is_disk_valid "${target_disk}" "${disks}")"; then
+      break
     else
       print --color red 'error: invalid disk. try again.\n\n' >&2
     fi
   done
 
-  local return_list=(
-    "${target_disk}"
-    "${root_partition}"
-    "${boot_partition}"
-  )
-
-  printf '%s\n' "${return_list[@]}"
+  printf '%s' "${target_disk}"
 }
 
 input_swap_size() {
@@ -303,19 +306,18 @@ input_swap_size() {
   while true; do
     print --color cyan 'enter swap size (e.g., "8G"): ' >&2
     read -r swap_size
-    printf '\n' >&2
+    print '\n' >&2
 
-    swap_size="${swap_size,,}"
     number="${swap_size%%[^[:digit:]]*}"
     suffix="${swap_size##*[[:digit:]]}"
 
-    if [[ "${swap_size}" == "${number}${suffix}" ]]; then
-      case "${suffix}" in
-      g | gib)
+    if [[ "${swap_size}" =~ ^[[:digit:]]+[[:alpha:]]+$ ]]; then
+      case "${suffix,,}" in
+      g | gb | gib)
         swap_size="${number}GiB"
         break
         ;;
-      m | mib)
+      m | mb | mib)
         swap_size="${number}MiB"
         break
         ;;
@@ -329,8 +331,14 @@ input_swap_size() {
 }
 
 input_reflector_options() {
+  local reflector_options=()
   local country=''
   local countries=''
+
+  countries="$(list_countries)" || {
+    print --color red 'error: failed to get countries.\n\n' >&2
+    return 1
+  }
 
   print --color cyan 'info: enter a country ' >&2
   print --color cyan 'to use as filter for reflector.\n' >&2
@@ -341,25 +349,17 @@ input_reflector_options() {
   while true; do
     print --color cyan 'enter a country (e.g., "Japan"): ' >&2
     read -r country
-    printf '\n' >&2
+    print '\n' >&2
 
     case "${country}" in
     l)
-      if countries="$(list_countries)"; then
-        printf '%s' "${countries}" |
-          column --fillrows |
-          less --clear-screen --tilde >&2
-      else
-        print --color red 'error: failed to get countries.\n\n' >&2
-        exit 1
-      fi
+      print "${countries}" |
+        column --fillrows |
+        less --clear-screen --tilde >&2
       ;;
     *)
-      if is_country_valid "${country}"; then
+      if country="$(is_country_valid "${country}" "${countries}")"; then
         break
-      elif [[ "$?" -eq 2 ]]; then
-        print --color red 'error: failed to get countries.\n\n' >&2
-        exit 1
       else
         print --color red 'error: invalid country. try again.\n\n' >&2
       fi
@@ -367,17 +367,44 @@ input_reflector_options() {
     esac
   done
 
-  if printf '%s' "${country}" | grep --quiet ' '; then
+  if [[ "${country}" == *' '* ]]; then
     country="'${country}'"
   fi
 
-  local return_list=(
+  reflector_options=(
     '--save' '/etc/pacman.d/mirrorlist'
     '--sort' 'score'
     '--country' "${country}"
   )
 
-  printf '%s\n' "${return_list[@]}"
+  printf '%s\n' "${reflector_options[@]}"
+}
+
+input_driver_packages() {
+  local driver_packages=()
+
+  if confirm 'install amd driver packages?' >&2; then
+    driver_packages+=("${AMD_DRIVER_PACKAGES[@]}")
+  fi
+
+  if confirm 'install intel driver packages?' >&2; then
+    driver_packages+=("${INTEL_DRIVER_PACKAGES[@]}")
+  fi
+
+  printf '%s\n' "${driver_packages[@]}"
+}
+
+input_optional_packages() {
+  local optional_packages=()
+  local package=''
+
+  for package in "${OPTIONAL_PACKAGES[@]}"; do
+    if confirm "install ${package}?" >&2; then
+      optional_packages+=("${package}")
+    fi
+  done
+
+  printf '%s\n' "${optional_packages[@]}"
 }
 
 # ------------------------------------------------------------------------------
@@ -410,12 +437,15 @@ sync_clock() {
 }
 
 partition_disk() {
-  local target_disk="$1"
-  local partition_layout='size=1GiB, type=uefi\n type=linux\n'
-  local sfdisk_options='--wipe always --wipe-partitions always --label gpt'
+  local disk="$1"
+  local partition_layout='size=1GiB, type=uefi\n type=linux'
+  local sfdisk_options=(
+    '--wipe' 'always'
+    '--wipe-partitions' 'always'
+    '--label' 'gpt'
+  )
 
-  printf '%s' "${partition_layout}" |
-    sfdisk "${sfdisk_options}" "${target_disk}" || return 1
+  sfdisk "${sfdisk_options[@]}" "${disk}" <<<"${partition_layout}" || return 1
 }
 
 format_partitions() {
@@ -470,7 +500,7 @@ install_system_packages() {
 }
 
 generate_fstab() {
-  genfstab -U /mnt >>/mnt/etc/fstab
+  genfstab -U /mnt >>/mnt/etc/fstab || return 1
 }
 
 configure_system() {
@@ -478,15 +508,11 @@ configure_system() {
   local copied_script_path=''
   local exit_status=0
 
-  source_script_path="$(dirname "$0")/configure.sh"
-  copied_script_path="/root/configure.sh"
+  source_script_path="${BASH_SOURCE%/*}/configure.sh"
+  copied_script_path='/root/configure.sh'
 
   cp --force "${source_script_path}" "/mnt/${copied_script_path}" || return 1
-
-  if ! arch-chroot /mnt /bin/bash "${copied_script_path}"; then
-    exit_status="$?"
-  fi
-
+  arch-chroot /mnt /bin/bash "${copied_script_path}" || exit_status=1
   rm --force "/mnt/${copied_script_path}" || return 1
   return "${exit_status}"
 }
@@ -511,15 +537,26 @@ is_connected() {
   fi
 }
 
+is_clock_synced() {
+  if [[ "$(timedatectl show -P NTPSynchronized)" == 'yes' ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 is_disk_valid() {
   local disk="$1"
-  local disks=''
+  local disks="$2"
+  local match=''
 
-  if ! disks="$(list_disks)"; then
-    return 2
-  fi
+  match="$(awk -v disk="${disk}" '
+    BEGIN { IGNORECASE = 1 }
+    $1 == disk { print $1 ; exit }
+  ' <<<"${disks}")"
 
-  if printf '%s' "${disks}" | grep --quiet "^${disk}\b"; then
+  if [[ -n "${match}" && "${match}" =~ ^/dev/(sd|nvme|mmcblk) ]]; then
+    printf '%s' "${match}"
     return 0
   else
     return 1
@@ -528,21 +565,16 @@ is_disk_valid() {
 
 is_country_valid() {
   local country="$1"
-  local countries=''
+  local countries="$2"
+  local match=''
 
-  if ! countries="$(list_countries)"; then
-    return 2
-  fi
+  match="$(awk -v country="${country}" '
+    BEGIN { IGNORECASE = 1 }
+    $0 == country { print $0 ; exit }
+  ' <<<"${countries}")"
 
-  if printf '%s' "${countries}" | grep --quiet "^${country}\$"; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-is_clock_synced() {
-  if [[ "$(timedatectl show -P NTPSynchronized)" == 'yes' ]]; then
+  if [[ -n "${match}" ]]; then
+    printf '%s' "${match}"
     return 0
   else
     return 1
@@ -572,22 +604,8 @@ print() {
   done
 
   if [[ -n "${color}" ]]; then
-    declare -A color_codes=(
-      [black]=30
-      [red]=31
-      [green]=32
-      [yellow]=33
-      [blue]=34
-      [magenta]=35
-      [cyan]=36
-      [white]=37
-    )
-
-    local color_sequence=''
-    local reset_sequence=''
-
-    color_sequence="\\033[1;${color_codes[${color}]}m"
-    reset_sequence='\033[0m'
+    local color_sequence="\\033[1;${COLOR_CODES[${color}]}m"
+    local reset_sequence='\033[0m'
 
     printf '%b' "${color_sequence}${message}${reset_sequence}"
   else
@@ -596,29 +614,24 @@ print() {
 }
 
 get_vendor_id() {
-  awk '/vendor_id/ { print $NF ; exit 1 }' /proc/cpuinfo
+  awk '/vendor_id/ { print $NF ; exit }' /proc/cpuinfo
 }
 
 list_disks() {
   local disks=''
   local lsblk_options=('--nodeps' '--noheadings' '--output' 'PATH,MODEL')
 
-  if ! disks="$(lsblk "${lsblk_options[@]}" 2>/dev/null)"; then
-    return 1
-  fi
+  disks="$(lsblk "${lsblk_options[@]}" 2>/dev/null)" || return 1
 
-  printf '%s' "${disks}" | awk '$1 ~ "^/dev/(sd|nvme|mmcblk)"'
+  awk '$1 ~ "^/dev/(sd|nvme|mmcblk)"' <<<"${disks}"
 }
 
 list_countries() {
   local countries=''
 
-  if ! countries="$(reflector --list-countries 2>/dev/null)"; then
-    return 1
-  fi
+  countries="$(reflector --list-countries 2>/dev/null)" || return 1
 
-  printf '%s' "${countries}" |
-    awk --field-separator='[ ]{2,}' 'FNR > 2 { print $1 }'
+  awk --field-separator='[ ]{2,}' 'FNR > 2 { print $1 }' <<<"${countries}"
 }
 
 main
